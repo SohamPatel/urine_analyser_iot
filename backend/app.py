6b0847b3d1d0312f4f5caacdc8f6807f62aa7ea8
Nick Balnaves <nickbalnaves@outlook.com>
2019-04-23 19:07:36 +1000

    deploy

--- backend/app.py
+++ backend/app.py
@@ -225,4 +225,5 @@ def dict_factory(cursor, row):
     return dictionary
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
